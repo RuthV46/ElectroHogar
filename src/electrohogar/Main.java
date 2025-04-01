cf5a3a053ae70717751f0627b0c2e5bfe41d22f2
RuthV46 <ruth.valan@gmail.com>
2025-04-01 14:20:50 -0500

    Modificacion de clase main

--- src/electrohogar/Main.java
+++ src/electrohogar/Main.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
+        scanner = new Scanner(System.in);
         Tienda tienda = new Tienda();
+        String opcionLavadora;
+        String opcionRefrigerador;
         
-        System.out.print("Ingrese la cantidad de lavadoras: ");
-        int numLavadoras = scanner.nextInt();
+        System.out.print("¿Desea ingresar una lavadora? SI/NO:  ");
+        opcionLavadora = scanner.next();
         scanner.nextLine();
         
-        for (int i = 0; i < numLavadoras; i++) {
+        while(!"NO".equals(opcionLavadora)){
+            
             System.out.print("Número de serie: ");
             String numeroSerie = scanner.nextLine();
             System.out.print("Marca: ");
@@ -23,17 +27,22 @@ public class Main {
             System.out.print("Capacidad de carga: ");
             double capacidad = scanner.nextDouble();
             scanner.nextLine();
-            
+
             tienda.agregarLavadora(new Lavadora(numeroSerie, marca, modelo, precio, capacidad));
-            
+
             System.out.println("<--->");
+            
+            System.out.print("¿Desea adicionar lavadora? SI/NO: ");
+            opcionLavadora = scanner.next();
+            scanner.nextLine();
         }
         
-        System.out.print("Ingrese la cantidad de refrigeradores: ");
-        int numRefrigeradores = scanner.nextInt();
+        System.out.print("¿Desea ingresar un refrigerador? SI/NO: ");
+        opcionRefrigerador = scanner.next();
         scanner.nextLine();
         
-        for (int i = 0; i < numRefrigeradores; i++) {
+        while(!"NO".equals(opcionRefrigerador)){
+                    
             System.out.print("Número de serie: ");
             String numeroSerie = scanner.nextLine();
             System.out.print("Marca: ");
@@ -49,7 +58,10 @@ public class Main {
             tienda.agregarRefrigerador(new Refrigerador(numeroSerie, marca, modelo, precio, tipoEnergia));
             
             System.out.println("<--->");
-        }
+            System.out.print("¿Desea adicionar refrigerador? SI/NO: ");
+            opcionRefrigerador = scanner.next();
+            scanner.nextLine();
+        } 
         
         System.out.println("Promedio de precios de lavadoras: " + tienda.calcularPromedioLavadoras());
         System.out.println("Promedio de precios de refrigeradores: " + tienda.calcularPromedioRefrigeradores());
